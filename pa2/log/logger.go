0b0b8576c5e6afada8234ee66603ffef9045df1c
Thanh Ngo <felix.infinite@gmail.com>
2016-07-28 23:38:02 -0400

    git/log: add color to the log message's header

--- pa2/log/logger.go
+++ pa2/log/logger.go
@@ -7,9 +7,9 @@ import (
 
 var (
 	// Warning log
-	Warning = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime)
+	Warning = log.New(os.Stdout, "\x1B[91mWARNING: \x1B[39m", log.Ldate|log.Ltime)
 	// Debug log
-	Debug = log.New(os.Stdout, "DEBUG:   ", log.Ldate|log.Ltime)
+	Debug = log.New(os.Stdout, "\x1B[96mDEBUG:   \x1B[39m", log.Ldate|log.Ltime)
 	// Info log
-	Info = log.New(os.Stdout, "INFO:    ", log.Ldate|log.Ltime)
+	Info = log.New(os.Stdout, "\x1B[92mINFO:    \x1B[39m", log.Ldate|log.Ltime)
 )
