aee75699493c4d61e05616b0101141309e5d69a1
Thanh Ngo <felix.infinite@gmail.com>
2016-07-28 23:11:48 -0400

    pa2/protocol: Add the sender package

---
+++ pa2/protocol/sender/sender.go
@@ -0,0 +1,46 @@
+package sender
+
+import (
+	"net"
+
+	"github.com/iocat/rutgers-cs352/pa2/protocol/datagram"
+)
+
+// Sender represents a sender that can sends data on UDP
+type Sender interface {
+	Broadcast()
+	SendTo(*net.UDPAddr)
+}
+
+// New creates a new sender
+func New(conn *net.UDPConn, packet *datagram.Segment) Sender {
+	if conn == nil {
+		panic("no connection is given to the sender.")
+	}
+	if packet == nil {
+		panic("no packet is given to the sender.")
+	}
+	return &udpBroadcaster{
+		packet: packet,
+		conn:   conn,
+	}
+}
+
+// udpBroadcaster is the concrete implementation of Sender
+type udpBroadcaster struct {
+	packet *datagram.Segment
+	// The UDP Socket
+	conn *net.UDPConn
+}
+
+func (sender udpBroadcaster) GoString() string {
+	return sender.packet.GoString()
+}
+
+func (sender *udpBroadcaster) Broadcast() {
+	sender.conn.Write(sender.packet.Bytes())
+}
+
+func (sender *udpBroadcaster) SendTo(addr *net.UDPAddr) {
+	sender.conn.WriteTo(sender.packet.Bytes(), addr)
+}
---
+++ pa2/protocol/sender/timeout.go
@@ -0,0 +1,62 @@
+package sender
+
+import (
+	"net"
+	"time"
+
+	"github.com/iocat/rutgers-cs352/pa2/log"
+	"github.com/iocat/rutgers-cs352/pa2/protocol/datagram"
+)
+
+// TimeoutSender represents a sender that after a certain amount of time
+// resend a same packet
+// To use: timeout := sender.TimeoutSender{
+//      Sender: sender.New(packet),
+//      Ticker: time.NewTicker(delay)
+// }
+// or
+// timeout := sender.NewTimeout(packet, duration)
+type TimeoutSender interface {
+	Start(addr *net.UDPAddr)
+	Stop()
+}
+
+type timeoutSender struct {
+	*time.Ticker
+	Sender
+}
+
+// NewTimeout creates a timeout sender given the packet and the duration that it takes
+func NewTimeout(conn *net.UDPConn, packet *datagram.Segment,
+	duration time.Duration) TimeoutSender {
+	return &timeoutSender{
+		Sender: New(conn, packet),
+		Ticker: time.NewTicker(duration),
+	}
+}
+
+// Start starts the timeout sender on another thread, which would periodically
+// forward the packet to the given address.
+// If addr is nil this method implicitly assumes the connection is a broadcast
+// Non-blocking call
+func (timeout *timeoutSender) Start(addr *net.UDPAddr) {
+	go func(addr *net.UDPAddr) {
+		var (
+			retransmit = false
+			timeoutS   = ""
+		)
+		for _ = range timeout.Ticker.C {
+			if retransmit {
+				timeoutS = "timeout: re"
+			}
+			if addr == nil {
+				log.Info.Printf("%sbroadcast packet %#v", timeoutS, timeout.Sender)
+				timeout.Broadcast()
+			} else {
+				log.Info.Printf("%stransmit packet %#v", timeoutS, timeout.Sender)
+				timeout.SendTo(addr)
+			}
+			retransmit = true
+		}
+	}(addr)
+}
