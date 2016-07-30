d22bf070440c0480ed66ebd307cbb00004f3840a
Thanh Ngo <felix.infinite@gmail.com>
2016-07-29 20:59:51 -0400

    pa2/prot/../header: change flag's methods receiver
    Change the receiver from pointer to struct because the methods do not
    edit the struct's states.

--- pa2/protocol/datagram/header/flag.go
+++ pa2/protocol/datagram/header/flag.go
@@ -31,49 +31,49 @@ var (
 type Flag uint8
 
 // Byte returns the byte representation of the flag
-func (flag *Flag) byte() byte {
-	return byte(*flag)
+func (flag Flag) byte() byte {
+	return byte(flag)
 }
 
 // IsRED returns whether this flag is a red
-func (flag *Flag) IsRED() bool {
-	return *flag&flagRED != 0
+func (flag Flag) IsRED() bool {
+	return flag&flagRED != 0
 }
 
 // IsFILE checks whether this flag is a file name
-func (flag *Flag) IsFILE() bool {
-	return *flag&flagFILE != 0
+func (flag Flag) IsFILE() bool {
+	return flag&flagFILE != 0
 }
 
 // IsNACK checks whether this flag is a NACK
-func (flag *Flag) IsNACK() bool {
-	return *flag&flagNACK != 0
+func (flag Flag) IsNACK() bool {
+	return flag&flagNACK != 0
 }
 
 // IsBLUE returns whether this flag is a blue
-func (flag *Flag) IsBLUE() bool {
-	return *flag&flagBLUE == 0
+func (flag Flag) IsBLUE() bool {
+	return flag&flagBLUE == 0
 }
 
 // IsACK returns whether this flag is an ACK or not
-func (flag *Flag) IsACK() bool {
-	return *flag&flagACK != 0
+func (flag Flag) IsACK() bool {
+	return flag&flagACK != 0
 }
 
 // IsEOF returns whether this flag is an EOF or not
-func (flag *Flag) IsEOF() bool {
-	return *flag&flagEOF != 0
+func (flag Flag) IsEOF() bool {
+	return flag&flagEOF != 0
 }
 
 // IsEXIT returns whether this flag is an Exit signal or not
-func (flag *Flag) IsEXIT() bool {
-	return *flag&flagEXIT != 0
+func (flag Flag) IsEXIT() bool {
+	return flag&flagEXIT != 0
 }
 
 // Is checks whether this flag contains the provided list of flags
-func (flag *Flag) Is(flags ...Flag) bool {
+func (flag Flag) Is(flags ...Flag) bool {
 	for _, f := range flags {
-		if *flag&f == 0 {
+		if flag&f == 0 {
 			return false
 		}
 	}
